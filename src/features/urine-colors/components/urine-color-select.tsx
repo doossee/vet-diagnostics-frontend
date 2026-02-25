c1e1b07ec098c5c2b6d436cadd0bd580e82d0e90
Nurbek <rustamovnurbek02@gmail.com>
2026-02-25 09:37:45 +0500

    adding type to urine color

--- src/features/urine-colors/components/urine-color-select.tsx
+++ src/features/urine-colors/components/urine-color-select.tsx
@@ -29,6 +29,7 @@ export function UrineColorSelect({ value, placeholder, disabled, onChange, onRem
       placeholder={placeholder}
       queryFn={(search) => useGetUrineColorsInfinite(search, undefined)}
       getOptionLabel={item => item?.[`name_${locale}`]}
+      extraLabel={item => item?.animalType?.[`name_${locale}`]}
       // clientSearch={(search, item) =>
       //   searchUtil(search, item, ["id", "name"])
       // }
